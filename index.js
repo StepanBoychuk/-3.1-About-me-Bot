0b05d1e16f28362879de784c034ae2d195aa4bc2
Stepan Boychuk <demonomiconnnn@gmail.com>
2024-01-05 12:54:32 +0200

    Changed response.js structure

--- index.js
+++ index.js
@@ -8,21 +8,23 @@ const token = process.env.TELEGRAM_TOKEN;
 
 const bot = new telegramBot(token, {polling: true});
 
+bot.on('polling_error', console.error) //Error handler in case of Syntax errors
+
 bot.onText(/\/start/, (msg) => {
     try {
-        bot.sendMessage(msg.chat.id, responses.startResponse, {
+        bot.sendMessage(msg.chat.id, responses.start.text, {
             'reply_markup': {
-                'keyboard': responses.keyboard
+                'keyboard': responses.start.options.keyboard
             }
         })
-    } catch (err) {
+    } catch (error) {
         console.error(error)
     }
 });
 
 bot.onText(/\/about/, (msg) => {
     try {
-        bot.sendMessage(msg.chat.id, responses.aboutResponse)
+        bot.sendMessage(msg.chat.id, responses.about.text)
     } catch (error) {
         console.error(error)
     }
@@ -30,7 +32,7 @@ bot.onText(/\/about/, (msg) => {
 
 bot.onText(/\/links/, (msg) => {
     try {
-        bot.sendMessage(msg.chat.id, responses.linksResponse)
+        bot.sendMessage(msg.chat.id, responses.links.text)
     } catch (error) {
         console.error(error)
     }
@@ -39,9 +41,9 @@ bot.onText(/\/links/, (msg) => {
 
 bot.onText(/\/help/, (msg) => {
     try {
-        bot.sendMessage(msg.chat.id, responses.helpResponse, {
+        bot.sendMessage(msg.chat.id, responses.help.text, {
             'reply_markup': {
-                'keyboard': responses.keyboard
+                'keyboard': responses.help.options.keyboard
             }
         })
     } catch (error) {
--- src/responses.js
+++ src/responses.js
@@ -1,20 +1,27 @@
 const keyboard = [['/about', '/links'], ['/help']];
 
-const startResponse = 'Welcome! Here is the list of available commands:';
+const responses = {
+    'start': {
+        'text': 'Welcome! Here is the list of available commands:',
+        'options': {
+            'keyboard': keyboard
+        }
+    },
+    'about': {
+        'text': `Hi! My name is Stepan. I'm trying to become a node.js developer and this is my first telegram bot`
+    },
+    'links': {
+        'text': `Here are some of my links:
+        \nGithub: https://github.com/StepanBoichuk
+        \n Linkedin: https://www.linkedin.com/in/stepan-boichuk-95aa4a248/ 
+         `
+    },
+    'help': {
+        'text': 'Here is the list of available commands:',
+        'options': {
+            'keyboard': [['/about', '/links'], ['/help']]
+        }
+    }
+};
 
-const aboutResponse = `Hi! My name is Stepan. I'm trying to become a node.js developer and this is my first telegram bot`;
-
-const linksResponse = `Here are some of my links:
-\nGithub: https://github.com/StepanBoichuk
-\n Linkedin: https://www.linkedin.com/in/stepan-boichuk-95aa4a248/ 
- `;
-
-const helpResponse =  'Here is the list of available commands:';
-
-module.exports = {
-    keyboard,
-    startResponse,
-    aboutResponse,
-    linksResponse,
-    helpResponse
-}
+module.exports = responses
