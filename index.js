cb35e88be500c62c55f2d68783fd47d878984e32
Stepan Boychuk <demonomiconnnn@gmail.com>
2024-01-08 17:49:37 +0200

    Used a loop to set up all of commands

--- index.js
+++ index.js
@@ -8,45 +8,19 @@ const token = process.env.TELEGRAM_TOKEN;
 
 const bot = new telegramBot(token, {polling: true});
 
-bot.on('polling_error', console.error) //Error handler in case of Syntax errors
+bot.on('polling_error', console.error); //Error handler in case of Syntax errors
 
-bot.onText(/\/start/, (msg) => {
-    try {
-        bot.sendMessage(msg.chat.id, responses.start.text, {
-            'reply_markup': {
-                'keyboard': responses.start.options.keyboard
-            }
-        })
-    } catch (error) {
-        console.error(error)
-    }
+Object.entries(responses).forEach(([command, response]) => {
+    const regexpCommand = new RegExp(command);
+    bot.onText(regexpCommand, (msg) => {
+        try {
+            bot.sendMessage(msg.chat.id, response.text, {
+                'reply_markup': {
+                    'keyboard': response.options.keyboard
+                }
+            })
+        }catch (error) {
+            console.error(error);
+        }
+    })
 });
-
-bot.onText(/\/about/, (msg) => {
-    try {
-        bot.sendMessage(msg.chat.id, responses.about.text)
-    } catch (error) {
-        console.error(error)
-    }
-})
-
-bot.onText(/\/links/, (msg) => {
-    try {
-        bot.sendMessage(msg.chat.id, responses.links.text)
-    } catch (error) {
-        console.error(error)
-    }
-})
-
-
-bot.onText(/\/help/, (msg) => {
-    try {
-        bot.sendMessage(msg.chat.id, responses.help.text, {
-            'reply_markup': {
-                'keyboard': responses.help.options.keyboard
-            }
-        })
-    } catch (error) {
-        console.error(error)
-    }
-})
